892f9925e72035ddebb9bf154a7f110af4faa44e
Rin Takanashi <dareka826@gmail.com>
2021-12-31 15:51:13 +0100

    Update install script to accept a base directory

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-install -Dm 644 nvidia-xrun-pm.service "/etc/systemd/system/nvidia-xrun-pm.service"
-install -Dm 664 nvidia-xrun-pm "/etc/init.d/nvidia-xrun-pm"
-install -Dm 644 config/nvidia-xrun "/etc/default/nvidia-xrun"
-install -Dm 644 nvidia-xorg.conf "/etc/X11/nvidia-xorg.conf"
-install -Dm 644 nvidia-xinitrc "/etc/X11/xinit/nvidia-xinitrc"
-install -Dm 755 nvidia-xrun "/usr/bin/nvidia-xrun"
-install -dm 555 "/etc/X11/xinit/nvidia-xinitrc.d"
-install -dm 555 "/etc/X11/nvidia-xorg.conf.d"
+DESTDIR="${DESTDIR:-}"
+
+install -Dm 644 nvidia-xrun-pm.service "${DESTDIR}/etc/systemd/system/nvidia-xrun-pm.service"
+install -Dm 664 nvidia-xrun-pm "${DESTDIR}/etc/init.d/nvidia-xrun-pm"
+install -Dm 644 config/nvidia-xrun "${DESTDIR}/etc/default/nvidia-xrun"
+install -Dm 644 nvidia-xorg.conf "${DESTDIR}/etc/X11/nvidia-xorg.conf"
+install -Dm 644 nvidia-xinitrc "${DESTDIR}/etc/X11/xinit/nvidia-xinitrc"
+install -Dm 755 nvidia-xrun "${DESTDIR}/usr/bin/nvidia-xrun"
+install -dm 555 "${DESTDIR}/etc/X11/xinit/nvidia-xinitrc.d"
+install -dm 555 "${DESTDIR}/etc/X11/nvidia-xorg.conf.d"
