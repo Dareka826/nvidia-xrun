f440fe495272569907c0385269bc250e41a461ed
Rin Takanashi <dareka826@gmail.com>
2021-12-31 15:57:41 +0100

    change permissions for openrc service in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 DESTDIR="${DESTDIR:-}"
 
 install -Dm 644 nvidia-xrun-pm.service "${DESTDIR}/etc/systemd/system/nvidia-xrun-pm.service"
-install -Dm 664 nvidia-xrun-pm "${DESTDIR}/etc/init.d/nvidia-xrun-pm"
+install -Dm 754 nvidia-xrun-pm "${DESTDIR}/etc/init.d/nvidia-xrun-pm"
 install -Dm 644 config/nvidia-xrun "${DESTDIR}/etc/default/nvidia-xrun"
 install -Dm 644 nvidia-xorg.conf "${DESTDIR}/etc/X11/nvidia-xorg.conf"
 install -Dm 644 nvidia-xinitrc "${DESTDIR}/etc/X11/xinit/nvidia-xinitrc"
